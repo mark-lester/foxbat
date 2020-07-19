7ee70eb75631a6ce4be53caaa73249faa83bf479
Mark Lester <mc_lester@yahoo.co.uk>
2020-07-19 16:34:48 +0700

    first working

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const Foxbat=require('../index')
+const foxbat = new Foxbat()
+const fs=require('fs').promises;
+const assert = require('assert')
+
+var context={
+	locale:'fr',
+	locale1:'LOCAL ONE',
+	locale2:'LOCAL TWO',
+}
+var testput
+fs.readFile('./test/testput.html')
+.then((t)=>{
+	testput=t
+	return foxbat.execute('./test/test.html','fr',context)
+	.then((output)=>{
+		describe('FILE 1', function () {
+			describe('Test 1', function () {
+				assert.equal(output,testput)
+			});
+		});
+	});
+})
